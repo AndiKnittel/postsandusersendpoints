7f84b319e2eca7acb2483f3891d51b819ebd2055
Andi <andiknittel.music@gmail.com>
2021-02-05 10:35:10 +0100

    created tokens controller / added middleware to all routes

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const usersController = require("../controllers/usersController");
 
+const tokensController = require("../controllers/tokensController");
+
+router.use(tokensController.verify);
+
 router.get("/", usersController.getUsers);
 router.get("/:id", usersController.getUserbyId);
 
